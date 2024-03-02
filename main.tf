b4d5dd5ef49cbcef9ac898a4a485440869e22fdc
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-02 13:43:56 -0300

    feat: add main.tf and variables

--- main.tf
+++ main.tf
@@ -1,50 +1,38 @@
-module "fiap-food-db" {
-  source  = "GoogleCloudPlatform/sql-db/google//modules/postgresql"
-  version = "13.0.1"
-
-  availability_type = "REGIONAL"
-  backup_configuration = {
-    enabled                        = true
-    start_time                     = "4:00" # 11PM EST  #specified in a 24-hour time format, in the UTC±00 time zone, and specifies the start of a 4-hour backup window
-    location                       = null
-    point_in_time_recovery_enabled = false
-    transaction_log_retention_days = null
-    retained_backups               = 365
-    retention_unit                 = "COUNT"
-  }
-  database_flags      = [{ name = "autovacuum", value = "off" }, { name = "max_connections", value = 300 }]
-  database_version    = "POSTGRES_14"
-  db_name             = "fiap-food"
-  deletion_protection = false
-  disk_size           = 10
-  enable_default_db   = true
-  enable_default_user = true
-  ip_configuration = {
-    ipv4_enabled        = false
-    require_ssl         = true
-    private_network     = var.private_network
-    allocated_ip_range  = null
-    authorized_networks = []
-  }
-  maintenance_window_day          = 7
-  maintenance_window_hour         = 12
-  maintenance_window_update_track = "stable"
-  name                            = var.database_instance_name
-  project_id                      = var.project_id
-  region                          = var.region
-  tier                            = var.database_tier
-  user_labels                     = module.labels.common_labels
-  user_name                       = "fiap"
-  zone                            = "us-east1-b"
+provider "google" {
+  project = var.GCP_ID
+  region  = var.DB_REGION
 }
 
-resource "kubernetes_secret" "fiap-food-db" {
-  metadata {
-    name      = "fiap-food-db"
-    namespace = kubernetes_namespace.defectdojo.metadata[0].name
-  }
+resource "google_sql_user" "mydatabase_user" {
+  name     = var.DB_USER
+  instance = google_sql_database_instance.mydatabase.name
+  password = var.DB_PASSWORD
+}
 
-  data = {
-    db_user_password = module.fiap-food-db.generated_user_password
+resource "google_sql_database_instance" "mydatabase" {
+  name             = var.DB_HOST
+  database_version = "MYSQL_8_0"
+  region           = var.DB_REGION
+
+  settings {
+    tier = var.DB_TIER
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "my-network"
+        value = "0.0.0.0/0"
+      }
+    }
+    # database_flags {
+    #   name  = "port"
+    #   value = var.DB_PORT
+    # }
   }
+}
+
+resource "google_sql_database" "mydatabase_db" {
+  name      = var.DB_HOST
+  instance  = google_sql_database_instance.mydatabase.name
+  charset   = "utf8"
+  collation = "utf8_general_ci"
 }
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "DB_PASSWORD" {
+  description = "Database root password"
+}
+
+variable "DB_USER" {
+  description = "Database root user"
+}
+
+variable "DB_PORT" {
+  description = "Database port"
+}
+
+variable "DB_HOST" {
+  description = "Database host"
+}
+
+variable "DB_REGION" {
+  description = "Database region"
+}
+
+variable "DB_TIER" {
+  description = "Database tier"
+}
+
+variable "GCP_ID" {
+  description = "Project ID"
+}
