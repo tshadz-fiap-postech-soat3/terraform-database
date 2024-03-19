08149455a472f1e471e1063768c4e7c2c210576c
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-19 08:46:13 -0300

    add settings

--- main.tf
+++ main.tf
@@ -22,6 +22,24 @@ resource "google_sql_database_instance" "mydatabase" {
   name             = var.DB_HOST
   database_version = "MYSQL_8_0"
   region           = var.DB_REGION
+  settings {
+    tier = var.DB_TIER
+    activation_policy = "ALWAYS"
+    ip_configuration {
+      ipv4_enabled    = true
+    }
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+      start_time = "23:00"
+    }
+    disk_autoresize = true
+    disk_size       = 10
+    maintenance_window {
+      day  = 1
+      hour = 0
+    }
+  }
 }
 
 resource "google_sql_database" "mydatabase_db" {
