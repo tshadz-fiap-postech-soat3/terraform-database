2518562df03fe4c6f470bd7732197c94e3c2254e
tshadz <thais.sombra@hotmail.com>
2024-07-06 15:25:30 -0300

    feat: deploy order api

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "fiap-postech-tsombra"
     workspaces {
-      name = "terraform-cloud-database-product-api"
+      name = "terraform-cloud-database-order-api"
     }
   }
 }
