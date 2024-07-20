5248c4bebac8d6c4cf728b8fa7a2df5b9d333385
tshadz <thais.sombra@hotmail.com>
2024-07-20 20:24:15 -0300

    fix: workspace name

--- set_workspace.sh
+++ set_workspace.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Substitua o nome do workspace no arquivo de configuração do Terraform
-sed -i "s/name = \"workspace\"/name = \"$TF_VAR_WORKSPACE\"/g" main.tf
+WORKSPACE_WITH_SUFFIX="${TF_VAR_WORKSPACE}-database"
+sed -i "s/name = \"workspace\"/name = \"$WORKSPACE_WITH_SUFFIX\"/g" main.tf
 
 cat main.tf
